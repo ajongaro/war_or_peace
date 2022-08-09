c3bf3398edc360bf47a4c7c2f79befd61b12f252
Anthony Ongaro <aongaro@gmail.com>
2022-08-09 13:03:44 -0700

    feat: class and argument testing

---
+++ spec/player_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+RSpec.describe Player do
+  it 'exists' do
+    player = Player.new('Anthony', :foo)
+    expect(player).to be_an_instance_of(Player)
+  end
+
+  it 'is initialized with a deck and name' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.name).to eq('Clarisa')
+    expect(player.deck.cards).to eq([card1, card2, card3])
+  end
+
+
+
+end
