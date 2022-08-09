2aba5f41055db31f7352a897421c0193bc3b7b15
Anthony Ongaro <aongaro@gmail.com>
2022-08-09 13:15:41 -0700

    feat: has_lost implementation and testing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ require './lib/player'
 
 RSpec.describe Player do
   it 'exists' do
-    player = Player.new('Anthony', :foo)
+    player = Player.new('Anthony', :FIXME)
     expect(player).to be_an_instance_of(Player)
   end
 
@@ -20,6 +20,21 @@ RSpec.describe Player do
     expect(player.deck.cards).to eq([card1, card2, card3])
   end
 
+  it 'recognizes when they have lost' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
 
+    expect(player.has_lost?).to be false
+
+    player.deck.remove_card
+    expect(player.has_lost?).to be false
+
+    2.times { player.deck.remove_card }
+    expect(player.has_lost?).to be true
+  end
+  
 
 end
