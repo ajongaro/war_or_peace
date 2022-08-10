24422e969eadbea1ea2f3e22abb31cdf589356a8
Anthony Ongaro <aongaro@gmail.com>
2022-08-09 17:15:02 -0700

    feat: shameless green type method logic and test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,17 +1,42 @@
 class Turn
 
   attr_reader :player1, :player2, :spoils_of_war
-
+  # initializes a turn with two players
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
   end
 
+  # determines which type of turn will occur
   def type
-    player1_card_rank = @player1.deck.cards[0].rank
-    player2_card_rank = @player2.deck.cards[0].rank
-    return :basic if player1_card_rank != player2_card_rank
-    
+    p1_card_1 = card_ranker(@player1, 0)
+    p1_card_3 = card_ranker(@player1, 2)
+    p2_card_1 = card_ranker(@player2, 0)
+    p2_card_3 = card_ranker(@player2, 2)
+
+    # logic for game type (shameless green)
+    if p1_card_1 == p2_card_1 # confirms a :war, checks for :destruction
+      if p1_card_3 == p2_card_3 && p1_card_3 != nil # checks if match and not nil
+        :mutually_assured_destruction
+      else
+        :war
+      end
+    else
+      :basic
+    end
+
   end
+
+
+  private
+  # ignores cards that don't exist and returns rank of rest
+  def card_ranker(player, index)
+    if player.deck.cards[index] == nil
+      nil
+    else
+      player.deck.cards[index].rank
+    end
+  end
+
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -33,4 +33,33 @@ RSpec.describe Turn do
 
     expect(turn.type).to eq(:basic)
   end
+
+  it 'is war type when ranks ranks are same' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Queen', 12)
+    card4 = Card.new(:club, '3', 3)
+    deck1 = Deck.new([card1, card2])
+    deck2 = Deck.new([card3, card4])
+    player1 = Player.new('Clarisa', deck1)
+    player2 = Player.new('Rachel', deck2)
+    turn = Turn.new(player1, player2)
+
+    expect(turn.type).to eq(:war)
+  end
+
+  it 'is destruction type when [0] and [2] are same rank' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Queen', 12)
+    card4 = Card.new(:club, '3', 3)
+    deck1 = Deck.new([card1, nil, card2])
+    deck2 = Deck.new([card3, nil, card4])
+    player1 = Player.new('Clarisa', deck1)
+    player2 = Player.new('Rachel', deck2)
+    turn = Turn.new(player1, player2)
+
+    expect(turn.type).to eq(:mutually_assured_destruction)
+  end
+
 end
